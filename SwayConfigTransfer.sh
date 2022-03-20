6128338ce133db0f3da649ae6f6900b6c28eeb6d
Marcus <hello@marcusskov.com>
2022-03-20 23:42:42 +0000

    bot: Updated dotfiles

--- SwayConfigTransfer.sh
+++ SwayConfigTransfer.sh
@@ -1,26 +1,29 @@
-# Copy Sway files to config repo
+# Copy Sway config
 cp ~/.config/sway/config ~/dotfiles-sway/.config/sway
 
-# Copy Waybar config files to git repo
+# Copy Waybar config
 cp ~/.config/waybar/* ~/dotfiles-sway/.config/waybar/
 
-# Copy Profile 
+# Copy Profile config
 cp /etc/environment ~/dotfiles-sway/etc/
 
-# Alacritty
+# Copy Alacritty config
 cp ~/.config/alacritty/* ~/dotfiles-sway/.config/alacritty/
 
-# Copy zshrc config file
+# Copy zshrc config
 cp ~/.zshrc ~/dotfiles-sway/
 
-# Copy NeoVim Config
+# Copy NeoVim config
 cp ~/.config/nvim/init.vim ~/dotfiles-sway/.config/nvim/
 
-# Copy Starship
+# Copy Starship config
 cp ~/.config/starship/config.toml ~/dotfiles-sway/.config/starship
 
-# Copy pacman config files
+# Copy pacman config
 cp /etc/pacman.conf ~/dotfiles-sway/etc/
+
+# Copy MangoHud config
+cp ~/etc/MangoHud/MangoHud.conf ~/dotfiles-sway/.config/MangoHud
 
 ###############
 # Git Add Files
@@ -28,4 +31,3 @@ cp /etc/pacman.conf ~/dotfiles-sway/etc/
 cd ~/dotfiles-sway/
 git add .
 git commit -m "bot: Updated dotfiles"
-git push
